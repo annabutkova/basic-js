99aebd062ad1cfaaca779baeae27b992326f5d48
Anna Butkova <44586010+annabutkova@users.noreply.github.com>
2021-03-29 02:33:47 +0500

    feat: solutions

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,6 +1,21 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function createDreamTeam(/* members */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function createDreamTeam(members) {
+  console.log(typeof(members));
+  if(typeof(members) == null) {
+    return false;
+    
+  }
+  
+  let teamName = '';
+  members.sort();
+  members.forEach(element => {
+    if(typeof(element) === 'string'){
+      teamName += element.trim().slice(0,1).toUpperCase();
+      teamName = teamName.split('').sort().join('');
+    } 
+    
+  });
+
+  return teamName;
 };
--- src/what-season.js
+++ src/what-season.js
@@ -1,6 +1,23 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function getSeason(/* date */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function getSeason(date) {
+  
+  if(date === 0) {
+    return 'Unable to determine the time of year!';
+  }
+  
+  let month = date.getMonth() + 1;
+  
+  if(month < 3 || month == 12) {
+    return 'winter'
+  } else if (month < 6) {
+    return 'spring';
+  } else if (month < 9) {
+    return 'summer';
+  } else if (month < 12){
+    return 'fall';
+  } else {
+    return 'Unable to determine the time of year!';
+  }
+  
 };
