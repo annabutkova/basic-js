25f37c2b6696477f1d86520a0d9d4e9b8fc5e86f
Anna Butkova <44586010+annabutkova@users.noreply.github.com>
2021-03-30 01:12:55 +0500

    feat: hanoi solution

---
+++ hanoi-tower.js
@@ -0,0 +1,12 @@
+const CustomError = require("../extensions/custom-error");
+
+module.exports = function calculateHanoi(disksNumber, turnsSpeed) {
+  
+  let turnsNum = (Math.pow(2, disksNumber)) - 1;
+  let time = Math.floor((turnsNum/turnsSpeed) * 3600);
+  
+  return {
+    turns: turnsNum, seconds: time
+  }
+  
+};
