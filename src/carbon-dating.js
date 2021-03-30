0e4d8db7b0295a6fcdf38525e27176c55dbd82e0
Anna Butkova <44586010+annabutkova@users.noreply.github.com>
2021-03-31 02:09:25 +0500

    feat: solution

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -4,5 +4,18 @@ const MODERN_ACTIVITY= 15;
 const HALF_LIFE_PERIOD= 5730;
 
 module.exports = function dateSample(sampleActivity) {
+    
+    if (typeof(sampleActivity) === 'string' &&  sampleActivity !== '' && MODERN_ACTIVITY > sampleActivity && sampleActivity > 0) {
+        
+    let k = 0.693/HALF_LIFE_PERIOD;
 
+    let age =  Math.ceil((Math.log(MODERN_ACTIVITY/sampleActivity))/k);
+    
+    return age.toFixed(0);
+    } else {
+        return false;
+    }
+    
+    
+    
 };
